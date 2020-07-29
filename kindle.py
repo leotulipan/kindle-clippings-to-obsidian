696314dc5b34d1c7429756cb3db3e1a2aedde906
Wesley Finck <wesleyfinck@gmail.com>
2020-07-29 18:50:43 -0300

    program creates highlights.json and notes.json with seemingly proper structure

--- kindle.py
+++ kindle.py
@@ -9,7 +9,9 @@ import re
 BOUNDARY = u"==========\r\n"
 DATA_FILE = u"clips.json"
 OUTPUT_DIR = u"output"
-
+HIGHLIGHT_FILE = u"highlights.json"
+NOTE_FILE = u"notes.json"
+ARTICLE_FILE = u"articles.json"
 
 def get_sections(filename):
     with open(filename, 'rb') as f:
@@ -18,7 +20,7 @@ def get_sections(filename):
     return content.split(BOUNDARY)
 
 
-def get_clip(section):
+def get_highlight_clip(section):
     clip = {}
 
     lines = [l for l in section.split(u'\r\n') if l]
@@ -29,9 +31,33 @@ def get_clip(section):
     match = re.search(r'(\d+)-\d+', lines[1])
     if not match:
         return
-    position = match.group(1)
+    # position = match.group(1)
+    position = match.group(0)
 
-    clip['position'] = int(position)
+    # clip['position'] = int(position)
+    clip['position'] = position
+    clip['content'] = lines[2]
+
+    return clip
+
+def get_note_clip(section):
+    clip = {}
+
+    lines = [l for l in section.split(u'\r\n') if l]
+    if len(lines) != 3:
+        return
+
+    clip['book'] = lines[0]
+    match = re.search('- Your Note on', lines[1])
+    if not match:
+        return
+    line = lines[1]
+    index = line.find('Location')
+    new_line = line[index:len(line)-1]
+    numbers = [int(s) for s in new_line.split() if s.isdigit()]
+    position = numbers[0]
+
+    clip['position'] = position
     clip['content'] = lines[2]
 
     return clip
@@ -51,42 +77,69 @@ def export_txt(clips):
             f.write("\n\n---\n\n".join(lines))
 
 
-def load_clips():
+def load_highlight_clips():
     """
-    Load previous clips from DATA_FILE
+    Load previous clips from HIGHLIGHT_FILE
     """
     try:
-        with open(DATA_FILE, 'rb') as f:
+        with open(HIGHLIGHT_FILE, 'rb') as f:
+            return json.load(f)
+    except (IOError, ValueError):
+        return {}
+
+def load_note_clips():
+    """
+    Load previous clips from NOTE_FILE
+    """
+    try:
+        with open(NOTE_FILE, 'rb') as f:
             return json.load(f)
     except (IOError, ValueError):
         return {}
 
 
-def save_clips(clips):
+def save_highlight_clips(clips):
     """
-    Save new clips to DATA_FILE
+    Save new clips to HIGHLIGHT_FILE
     """
-    with open(DATA_FILE, 'wb') as f:
+    with open(HIGHLIGHT_FILE, 'wb') as f:
+        json.dump(clips, f)
+
+def save_note_clips(clips):
+    """
+    Save new clips to NOTE_FILE
+    """
+    with open(NOTE_FILE, 'wb') as f:
         json.dump(clips, f)
 
 
 def main():
+    # for highlights
     # load old clips
     clips = collections.defaultdict(dict)
-    clips.update(load_clips())
+    clips.update(load_highlight_clips())
 
+    n_clips = collections.defaultdict(dict)
+    n_clips.update(load_note_clips())
     # extract clips
     sections = get_sections(u'My Clippings.txt')
     for section in sections:
-        clip = get_clip(section)
+        clip = get_highlight_clip(section)
         if clip:
             clips[clip['book']][str(clip['position'])] = clip['content']
+        
+        n_clip = get_note_clip(section)
+        if n_clip:
+            n_clips[n_clip['book']][str(n_clip['position'])] = n_clip['content']
 
     # remove key with empty value
     clips = {k: v for k, v in clips.items() if v}
+    n_clips = {x: y for x, y in n_clips.items() if y}
 
     # save/export clips
-    save_clips(clips)
+    save_highlight_clips(clips)
+    save_note_clips(n_clips)
+    
     export_txt(clips)
 
 
